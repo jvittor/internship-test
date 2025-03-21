5f5dfc2556fcbea4060945b4ed2156fb5c03a272
VYT <102699222+jvittor@users.noreply.github.com>
2025-03-21 14:12:27 +0000

    refactor(components): add comments in posts

--- src/lib/pages/home/components/posts-card.tsx
+++ src/lib/pages/home/components/posts-card.tsx
@@ -1,3 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+'use client';
+
 import * as React from 'react';
 import {
   Avatar,
@@ -6,11 +9,37 @@ import {
   CardActions,
   Typography,
   IconButton,
+  CircularProgress,
 } from '@mui/material';
 import { FavoriteBorder, ChatBubbleOutline, Share } from '@mui/icons-material';
 import { Post } from '@/lib/entities/home/post';
+import { Comment } from '@/lib/entities/home/comments';
+import { CommentUseCases } from '@/lib/usecases/comments.usecases';
 
 const PostCard = ({ post }: { post: Post }) => {
+  const [comments, setComments] = React.useState<Comment[]>([]);
+  const [loadingComments, setLoadingComments] = React.useState<boolean>(false);
+  const [showComments, setShowComments] = React.useState<boolean>(false);
+  const [error, setError] = React.useState<string | null>(null);
+
+  const loadComments = async () => {
+    setLoadingComments(true);
+    try {
+      const commentsData = await CommentUseCases.getComments(post.id);
+      setComments(commentsData);
+    } catch (error) {
+      setError('Falha ao carregar comentários');
+    } finally {
+      setLoadingComments(false);
+    }
+  };
+  const toggleComments = () => {
+    if (!showComments) {
+      loadComments();
+    }
+    setShowComments(!showComments);
+  };
+
   return (
     <Card sx={{ mb: 2 }}>
       <CardContent sx={{ display: 'flex', alignItems: 'center' }}>
@@ -33,7 +62,7 @@ const PostCard = ({ post }: { post: Post }) => {
           <Typography variant="body2">35</Typography>
         </div>
         <div>
-          <IconButton>
+          <IconButton onClick={toggleComments}>
             <ChatBubbleOutline />
           </IconButton>
           <Typography variant="body2">12</Typography>
@@ -45,6 +74,26 @@ const PostCard = ({ post }: { post: Post }) => {
           <Typography variant="body2">7</Typography>
         </div>
       </CardActions>
+
+      {showComments && (
+        <CardContent>
+          {loadingComments ? (
+            <CircularProgress size={24} />
+          ) : error ? (
+            <Typography color="error">{error}</Typography>
+          ) : comments.length > 0 ? (
+            comments.map((comment) => (
+              <Typography key={comment.id} variant="body2">
+                {comment.body}
+              </Typography>
+            ))
+          ) : (
+            <Typography variant="body2" color="textSecondary">
+              Nenhum comentário disponível
+            </Typography>
+          )}
+        </CardContent>
+      )}
     </Card>
   );
 };
