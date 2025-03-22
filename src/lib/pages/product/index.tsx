82f7c87ffbf0ebed092cb6fd644db3808c4112c7
VYT <102699222+jvittor@users.noreply.github.com>
2025-03-22 20:25:27 +0000

    fix(pages): pageprops type

--- src/lib/pages/product/index.tsx
+++ src/lib/pages/product/index.tsx
@@ -1,41 +1,33 @@
+/* eslint-disable @next/next/no-async-client-component */
 'use client';
 
-import { useEffect, useState } from 'react';
 import { CategoriesUseCases } from '@/lib/usecases/categories.usecases';
 import { Categories } from '@/lib/entities/home/categories';
 import ProductList from '@/lib/pages/product/components/product';
 import Breadcrumbs from '@/lib/components/breadcrumbs';
 
-type CategoryPageProps = {
-  params: {
-    category: string;
-  };
-};
+type Params = Promise<{ category: string }>;
 
-export default function CategoryPage({ params }: CategoryPageProps) {
-  const category = decodeURIComponent(params.category);
-  const [products, setProducts] = useState<Categories[]>([]);
+export default async function CategoryPage({ params }: { params: Params }) {
+  const { category } = await params;
+  const decodedCategory = decodeURIComponent(category);
 
-  useEffect(() => {
-    const fetchProducts = async () => {
-      try {
-        console.log('Buscando produtos para categoria:', category);
-        const data = await CategoriesUseCases.getCategories(category);
-        setProducts(data);
-      } catch (error) {
-        console.error('Erro ao buscar produtos:', error);
-      }
-    };
-
-    fetchProducts();
-  }, [category]);
+  let products: Categories[] = [];
+  try {
+    console.log('Buscando produtos para categoria:', decodedCategory);
+    products = await CategoriesUseCases.getCategories(decodedCategory);
+  } catch (error) {
+    console.error('Erro ao buscar produtos:', error);
+  }
 
   return (
     <>
-      <Breadcrumbs items={[{ label: category }]} />
+      <Breadcrumbs items={[{ label: decodedCategory }]} />
       <div className="flex w-full items-center justify-center">
         <div>
-          <h1 className="mb-4 text-2xl font-bold">Category: {category}</h1>
+          <h1 className="mb-4 text-2xl font-bold">
+            Category: {decodedCategory}
+          </h1>
           <ProductList products={products} />
         </div>
       </div>
